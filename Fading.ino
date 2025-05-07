5116a55649d6b4e7215c29a94f79a08637eb7b83
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-08 06:33:20 +0700

    Add files via upload

---
+++ Fading.ino
@@ -0,0 +1,23 @@
+/*
+FADING
+
+Author: RAFIQUL HAKIM
+Date/Time : Minggu/16 Desember 2018
+*/
+
+int ledPin = 9;
+
+void setup() {
+}
+
+void loop() {
+  for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
+    analogWrite(ledPin, fadeValue);
+    delay(30);
+  }
+
+  for (int fadeValue = 255; fadeValue >= 0; fadeValue -= 5) {
+    analogWrite(ledPin, fadeValue);
+    delay(30);
+  }
+}
